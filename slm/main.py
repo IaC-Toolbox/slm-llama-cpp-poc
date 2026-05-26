02b40cd22dda67f1c1c5af0bd7ac5f2a3bbdab9a
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2026-05-26 14:15:24 +0100

    handle imports

--- slm/main.py
+++ slm/main.py
@@ -8,16 +8,18 @@ from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter  # changed: grpc -> http
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from phoenix.otel import register
-from slm.routes.routes import create_router
 
-import logging
+from phoenix.otel import register
 
 tracer_provider = register(
   project_name="default",
   auto_instrument=True,
   endpoint=os.getenv("PHOENIX_COLLECTOR_ENDPOINT", "http://localhost:4318/v1/traces")
 )
+
+from slm.routes.routes import create_router
+
+import logging
 
 
 logging.basicConfig(level=logging.DEBUG)
